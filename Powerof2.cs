c52652059f6ac79e84ec0e432bc21ed5b142afdf
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-02-18 02:41:55 +0530

    pushing Program.cs and Powerof2.cs file

---
+++ Powerof2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlipCoin
+{
+    public class Powerof2
+    {
+        public static void Power()
+        {
+            Console.WriteLine(powertwo(32));
+            Console.ReadLine();
+        }
+        static bool powertwo(ulong input)
+        { 
+            return (input&(input-1))==0;
+
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FlipCoin
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Enter the value for number of flips ");
+            //int flipCoin = Convert.ToInt32(Console.ReadLine());
+            //FlipCoin.FlipTheCoin(flipCoin);
+            // HermonicNumber.HarmonicNum();
+            //Factors.PrimeFact();
+            Powerof2.Power();
+        }
+    }
+}
