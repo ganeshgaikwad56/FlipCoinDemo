0fe07837b516d5300d058c9d81eecb0b73cd9fb6
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-02-23 22:06:39 +0530

    Pushing Quotient and Reminder file

---
+++ SwichCase/QuotientAndRemindere.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwichCase
+{
+    public class QuotientAndRemindere
+    {
+        public static void FindQuotientAndRemindere()
+        {
+            int divident = 78, divisor = 8;
+
+            int quotient = divident / divisor;
+            int reminder=divident % divisor;
+
+            Console.WriteLine("Divident : {0}, Divisor: {1}",divident, divisor);
+            Console.WriteLine("Quotient = " + quotient);
+            Console.WriteLine("Reminder = " + reminder);
+        }
+
+    }
+}
