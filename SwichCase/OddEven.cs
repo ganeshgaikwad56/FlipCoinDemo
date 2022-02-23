7536c1e938569813a52c6d121aa90474159324be
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-02-23 22:47:43 +0530

    Adding Odd Even Number Program File

---
+++ SwichCase/OddEven.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwichCase
+{
+    public class OddEven
+    {
+        public static void OddandEven()
+
+        {
+            Console.WriteLine("Enter The Number");
+
+            int N =Convert.ToInt32(Console.ReadLine());
+
+            if (N%2==0)
+            {
+                Console.WriteLine("The number is even");
+            }
+            else
+            {
+                Console.WriteLine("The number is odd");
+            }
+        }
+    }
+}
