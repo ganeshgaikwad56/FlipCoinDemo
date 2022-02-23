9f84a4d504b337c3fb903fc2c34b3e6a60d06ee7
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-02-23 22:43:44 +0530

    Adding Swap Number Program File

---
+++ SwichCase/SwapTheNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwichCase
+{
+    public class SwapTheNumber
+    {
+        public static void CheckSwapTheNumber()
+        {
+            int num1 = 20, num2 = 60;
+            Console.WriteLine("Before Swap Num1= "+num1+ " Num2= "+num2);
+            num1 = num1 * num2;
+            num2 = num1/num2;
+            num1 = num1 / num2;
+            Console.WriteLine("After Swap num1= " + num1 + " Num2= " + num2);
+
+        }
+    }
+}
