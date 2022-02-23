ce13364606808d668f5954354af572851e3882f0
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-02-23 22:49:15 +0530

    Adding Program.s for all Program File

---
+++ SwichCase/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SwichCase
+{
+    class Program
+
+    {
+        static void Main(String[] args)
+        {
+            //Switch.DisplayMonth();
+
+            // OddEven.OddandEven();
+
+            // VowelsAndConstant.CheckVowelsAndConstant();
+            //SwapTheNumber.CheckSwapTheNumber();
+            // FindingLargest.FindLargestNumber();
+            QuotientAndRemindere.FindQuotientAndRemindere();
+        }
+    }
+}
