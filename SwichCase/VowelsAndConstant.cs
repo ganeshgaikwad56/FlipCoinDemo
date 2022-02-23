688be397ab814fc79ab148c96fc3d103eb6f1838
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-02-23 22:50:51 +0530

    Adding Vowels And Constant Program File

---
+++ SwichCase/VowelsAndConstant.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwichCase
+{
+    public class VowelsAndConstant
+    {
+        public static void CheckVowelsAndConstant()
+        {
+            Console.WriteLine("Enter any Charecter");
+            char charecter=Convert.ToChar(Console.ReadLine());
+            if (charecter == 'a' || charecter == 'e' || charecter == 'i' || charecter == 'o' || charecter == 'u' || charecter == 'A' || charecter == 'E' || charecter == 'I' || charecter == 'O' || charecter == 'U') 
+            {
+                Console.WriteLine(charecter+ " is Vowel");
+            }
+            else if ((charecter>='a' && charecter<='z')||(charecter>='A' && charecter<='Z'))
+            {
+                Console.WriteLine(charecter + " Is Constant");
+            }
+            else
+            {
+                Console.WriteLine("Enter Valid Charecter");
+            }
+        }
+    }
+}
